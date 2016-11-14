ca049f0a5c094e9855a5f2337b740ec0ee22d988
Teerachai <teerachai@perfenterprise.com>
2016-11-14 10:58:15 +0700

    First spike with SoundPlayer.

--- MyAlarm/UniAlarm/Program.cs
+++ MyAlarm/UniAlarm/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace UniAlarm
     {
         static void Main(string[] args)
         {
+            var player = new SoundPlayer("http://www.wavsource.com/snds_2016-10-30_1570758759693582/movies/aladdin/aladdin_fast.wav");
+            player.Play();
+
+            Console.ReadLine();
+
+            var play2 = new SoundPlayer(@"D:\src\git\my-alarm\assets\aladdin_who_disturbs.wav");
+            play2.Play();
+
+            Console.ReadLine();
         }
     }
 }
