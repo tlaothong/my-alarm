8004e0429a52874fe147c69356f2d99d5a6036c6
Teerachai <teerachai@perfenterprise.com>
2016-11-14 12:12:43 +0700

    Init Test Structure and Spike some concepts

---
+++ MyAlarm/Tests/UniAlarmTests/Tests/AlarmParserTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UniAlarm.Tests
+{
+    [TestClass]
+    public class AlarmParserTests
+    {
+        [TestMethod]
+        public void ParseTimeSpan()
+        {
+            var result = TimeSpan.Parse("10:30");
+            Assert.AreEqual(10, result.Hours, "Hours");
+            Assert.AreEqual(30, result.Minutes, "Minutes");
+        }
+
+        [TestMethod]
+        public void TryParseTimeSpan()
+        {
+            TimeSpan result;
+            var parseSucceeded = TimeSpan.TryParse("12:45", out result);
+            Assert.IsTrue(parseSucceeded);
+            Assert.AreEqual(12, result.Hours, "Hours");
+            Assert.AreEqual(45, result.Minutes, "Minutes");
+        }
+
+        [TestMethod]
+        public void CreateAlarmParser()
+        {
+            var configpath = "some_path_to_config_file";
+            var parser = new AlarmParser(configpath);
+            Assert.AreEqual(configpath, parser.ConfigFileFullPath, "Config Path");
+        }
+    }
+}
---
+++ MyAlarm/UniAlarm/AlarmParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniAlarm
+{
+    internal class AlarmParser
+    {
+        public readonly string ConfigFileFullPath;
+
+        public AlarmParser(string fullPathToConfigFile)
+        {
+            this.ConfigFileFullPath = fullPathToConfigFile;
+        }
+    }
+}
